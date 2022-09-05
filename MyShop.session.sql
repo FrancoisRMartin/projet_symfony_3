b3d7b975614f937e22baed4b3fe53d78864c79c5
FrancoisRMartin <f.r.martin1995@gmail.com>
2022-09-05 04:01:18 +0200

    Modification avant envoi

---
+++ MyShop.session.sql
@@ -0,0 +1,5 @@
+INSERT INTO `commande` (`id`, `membre_id`, `produit_id`, `quantite`, `montant`, `etat`, `date_enregistrement`) VALUES
+(1, 1, 1, 2, 32, 'En traitement', '2022-09-02 22:10:13'),
+(2, 2, 1, 6, 72, 'Payement confirmé', '2022-09-02 22:10:13'),
+(3, 1, 2, 3, 48, 'Livraison en cours', '2022-09-04 20:57:04'),
+(4, 1, 2, 3, 48, 'Produit livré', '2022-09-04 20:57:04');
